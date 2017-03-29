f51dab89b49ff36a2d693e7a3da9be5a830bb57c
sunwandi <nanyin194@163.com>
2017-03-29 18:40:52 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 #获取 token
-ans=$(curl -s -X POST -H "Content-Type: application/json" -d '{  "app_key": "fbfa832dee2b434582cf726078976543",  "app_secret": "495567f8becd457e971937133579a36b"}' "https://open.c.163.com/api/v1/token")
+ans=$(curl -s -X POST -H "Content-Type: application/json" -d '{  "app_key": "8b575101f6504bc6b81865d5baeaf7a4",  "app_secret": "e6137ac7c6dc4f47aec40e834450a1e6"}' "https://open.c.163.com/api/v1/token")
 token=`echo $ans | awk -F ',' '{print $1}' | awk -F ':' '{print $2}' | sed 's/^.//' | sed 's/.$//'`
 echo "token: $token"
 
